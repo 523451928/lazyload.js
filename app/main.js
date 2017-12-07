4a7d9f158cf0f481d3821a094e594a0126c98fd4
unknown <523451928@qq.com>
2017-12-07 21:36:54 +0800

    update

--- app/main.js
+++ app/main.js
@@ -2,6 +2,7 @@ import './main.css'
 import greeter from './js/Greeter'
 import LazyLoad from './js/lazyload'
 new LazyLoad('img',{
-  src: 'data-src'
+  src: 'data-src',
+  delay: 600
 })
 document.querySelector("#root").appendChild(greeter());
